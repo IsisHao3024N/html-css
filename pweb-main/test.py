7d96539daafd58f3f6a5a7ad531948c383c9a1fa
Izadora <izadoramariano323@gmail.com>
2024-11-14 14:10:40 -0300

    .

---
+++ pweb-main/test.py
@@ -0,0 +1,20 @@
+#nome= 'Guanabara'
+#idade = 25
+#peso = 75.5
+
+#nome = input('Qual seu nome?')
+#idade = input('Quantos anos vc tem?')
+#peso = input('Qual o seu peso?')
+#print(nome,idade,peso)
+
+#nome =input('Qual seu nome?')
+#print('Seja bem vindo(a)', nome)
+#dia = input('Qual o dia do seu nascimento?')
+#mes = input('Qual o mês do seu nasciimento?')
+#ano =input('Qual o ano do seu nascimento?')
+#print('A data completa do seu nascimenton é dia',  dia, 'do mês de', mes,'de',ano)
+
+n1 = input('Digite um número:')
+n2 = input('Digite outro número:')
+soma = (n1 + n2)
+print('A soma desses números é', soma)
